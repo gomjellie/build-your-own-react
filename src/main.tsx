64a3905451bfde21f123d1209bf749c16e23f691
gomjellie <gomjellie@gmail.com>
2022-12-04 17:53:40 +0900

    Fiber 타입 맨 위로 올림

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 // prettier-ignore
 type TAG_NAMES = "object" | "a" | "abbr" | "address" | "area" | "article" | "aside" | "audio" | "b" | "base" | "bdi" | "bdo" | "big" | "blockquote" | "body" | "br" | "button" | "canvas" | "caption" | "center" | "cite" | "code" | "col" | "colgroup" | "data" | "datalist" | "dd" | "del" | "details" | "dfn" | "dialog" | "div" | "dl" | "dt" | "em" | "embed" | "fieldset" | "figcaption" | "figure" | "footer" | "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "head" | "header" | "hgroup" | "hr" | "html" | "i" | "iframe" | "img" | "input" | "ins" | "kbd" | "keygen" | "label" | "legend" | "li" | "link" | "main" | "map" | "mark" | "menu" | "menuitem" | "meta" | "meter" | "nav" | "noscript" | "ol" | "optgroup" | "option" | "output" | "p" | "param" | "picture" | "pre" | "progress" | "q" | "rp" | "rt" | "ruby" | "s" | "samp" | "slot" | "script" | "section" | "select" | "small" | "source" | "span" | "strong" | "style" | "sub" | "summary" | "sup" | "table" | "template" | "tbody" | "td" | "textarea" | "tfoot" | "th" | "thead" | "time" | "title" | "tr" | "track" | "u" | "ul" | "var" | "video" | "wbr" | "webview";
 
+type Fiber = {
+  type: TAG_NAMES | 'TEXT_ELEMENT';
+  dom: Text | HTMLElement | null;
+  parent: Fiber | null;
+  child: Fiber | null;
+  sibling: Fiber | null;
+  props: {
+    children: JSX.Element[];
+  } & Record<string, any>;
+};
+
 function createElement(
   type: TAG_NAMES,
   props: Record<string, any>,
@@ -116,17 +127,6 @@ function performUnitOfWork(fiber: Fiber) {
   }
   return null;
 }
-
-type Fiber = {
-  type: TAG_NAMES | 'TEXT_ELEMENT';
-  dom: Text | HTMLElement | null;
-  parent: Fiber | null;
-  child: Fiber | null;
-  sibling: Fiber | null;
-  props: {
-    children: JSX.Element[];
-  } & Record<string, any>;
-};
 
 function createDom(fiber: Fiber): Text | HTMLElement {
   const dom =
