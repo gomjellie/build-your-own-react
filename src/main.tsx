2594eda24f1b8bdaedc09572aca41b90864ab493
gomjellie <gomjellie@gmail.com>
2022-12-04 12:01:23 +0900

    react import 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import type { ReactHTML } from 'react';
-
-type HTML_TYPES = keyof ReactHTML;
+// prettier-ignore
+type TAG_NAMES = "object" | "a" | "abbr" | "address" | "area" | "article" | "aside" | "audio" | "b" | "base" | "bdi" | "bdo" | "big" | "blockquote" | "body" | "br" | "button" | "canvas" | "caption" | "center" | "cite" | "code" | "col" | "colgroup" | "data" | "datalist" | "dd" | "del" | "details" | "dfn" | "dialog" | "div" | "dl" | "dt" | "em" | "embed" | "fieldset" | "figcaption" | "figure" | "footer" | "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "head" | "header" | "hgroup" | "hr" | "html" | "i" | "iframe" | "img" | "input" | "ins" | "kbd" | "keygen" | "label" | "legend" | "li" | "link" | "main" | "map" | "mark" | "menu" | "menuitem" | "meta" | "meter" | "nav" | "noscript" | "ol" | "optgroup" | "option" | "output" | "p" | "param" | "picture" | "pre" | "progress" | "q" | "rp" | "rt" | "ruby" | "s" | "samp" | "slot" | "script" | "section" | "select" | "small" | "source" | "span" | "strong" | "style" | "sub" | "summary" | "sup" | "table" | "template" | "tbody" | "td" | "textarea" | "tfoot" | "th" | "thead" | "time" | "title" | "tr" | "track" | "u" | "ul" | "var" | "video" | "wbr" | "webview";
 
 function createElement(
-  type: HTML_TYPES,
+  type: TAG_NAMES,
   props: Record<string, any>,
   ...children: JSX.Element[]
 ): JSX.Element {
@@ -95,7 +94,7 @@ function performUnitOfWork(fiber: Fiber) {
 }
 
 type Fiber = {
-  type: HTML_TYPES | 'TEXT_ELEMENT';
+  type: TAG_NAMES | 'TEXT_ELEMENT';
   dom: Text | HTMLElement | null;
   parent: Fiber | null;
   child: Fiber | null;
